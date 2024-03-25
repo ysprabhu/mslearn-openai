5cb4ea8528a344baf112684075e046ee9f041615
Yogesh Prabhu <yogeshprabhu@hotmail.com>
2024-03-24 22:05:52 -0700

    Try exercise

--- Labfiles/02-azure-openai-api/CSharp/Program.cs
+++ Labfiles/02-azure-openai-api/CSharp/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration.Json;
 using Azure;
 
 // Add Azure OpenAI package
+using Azure.AI.OpenAI;
 
 
 // Build a config object and retrieve user settings.
@@ -23,8 +24,16 @@ if(string.IsNullOrEmpty(oaiEndpoint) || string.IsNullOrEmpty(oaiKey) || string.I
 }
 
 // Initialize the Azure OpenAI client...
+var client = new OpenAIClient(new Uri(oaiEndpoint), new AzureKeyCredential(oaiKey));
 
+// System message to provide context to the model
+string systemMessage = "I am a hiking enthusiast named Forest who helps people discover hikes in their area. If no area is specified, I will default to near Rainier National Park. I will then provide three suggestions for nearby hikes that vary in length. I will also share an interesting fact about the local nature on the hikes when making a recommendation.";
 
+// Initialize messages list
+var messagesList = new List<ChatRequestMessage>()
+ {
+     new ChatRequestSystemMessage(systemMessage),
+ };
 
 do {
     Console.WriteLine("Enter your prompt text (or type 'quit' to exit): ");
@@ -41,6 +50,43 @@ do {
 
     // Add code to send request...
 
+    // Part 1 - Without conversation context
+    //var chatCompletionsOptions = new ChatCompletionsOptions
+    //{
+    //    Messages =
+    //    {
+    //        new ChatRequestSystemMessage(systemMessage),
+    //        new ChatRequestUserMessage(inputText)
+    //    },
+    //    MaxTokens = 400,
+    //    Temperature = 0.7f,
+    //    DeploymentName = oaiDeploymentName
+    //};
 
+    // Part 2 - With conversation context maintained in a list
+    messagesList.Add(new ChatRequestUserMessage(inputText));
 
+    var chatCompletionsOptions = new ChatCompletionsOptions()
+    {
+        MaxTokens = 1200,
+        Temperature = 0.7f,
+        DeploymentName = oaiDeploymentName
+    };
+
+    // Add messages to the completion options
+    foreach (ChatRequestMessage chatMessage in messagesList)
+    {
+        chatCompletionsOptions.Messages.Add(chatMessage);
+    }
+
+    // Send request to Azure OpenAI model
+    ChatCompletions chatCompletions = client.GetChatCompletions(chatCompletionsOptions);
+
+    // Return the response
+    var messageContent = chatCompletions.Choices[0].Message.Content;
+
+    // Add generated text to messages list
+    messagesList.Add(new ChatRequestAssistantMessage(messageContent));
+
+    Console.WriteLine($"Response: {messageContent}\n");
 } while (true);
